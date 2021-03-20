5ec351432681953cdab27efcfbd9df1e8e1cab64
Kimi Zou <kimizou.kz@gmail.com>
2021-03-20 18:00:11 -0400

    Set up layout

--- src/App.js
+++ src/App.js
@@ -1,23 +1,41 @@
-import logo from './logo.svg';
+import { Button, Layout, Menu, Breadcrumb } from 'antd';
+import { UserOutlined, LaptopOutlined, NotificationOutlined } from '@ant-design/icons';
+import { Redirect, Route, Switch } from 'react-router-dom';
+
+import Navigation from './components/Navigation';
+import Sidebar from './components/Sidebar';
+
 import './App.css';
 
-function App() {
+const { SubMenu } = Menu;
+const { Header, Content, Sider } = Layout;
+
+function App () {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <Layout>
+        <Navigation />
+        <Layout>
+          <Sidebar />
+          <Layout style={{ padding: '0 24px 24px' }}>
+            <Content
+              className='site-layout-background'
+              style={{
+                padding: 24,
+                margin: 0,
+                minHeight: 280
+              }}
+            >
+              Content
+              <Switch>
+                <Route exact path='/'>
+                  {/* <Home /> */}
+                </Route>
+              </Switch>
+            </Content>
+          </Layout>
+        </Layout>
+      </Layout>
     </div>
   );
 }
